b13d492c56c83d725e3b4af55a9fc7ff403c88f1
Dave Weekes <dweekes@jdmsoftware.com.au>
2016-10-25 17:04:14 +1000

    Added Header STuff

---
+++ GameTools/GameTools/classes/GameToolsPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameTools.classes
+{
+    public class GameToolsPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Web.UI.HtmlControls.HtmlControl jsJquery;// = System.Web.UI.HtmlControls.HtmlControl("script");
+            jsJquery.Attributes.Add("type", "text/javascript");
+            jsJquery.Attributes.Add("language", "javascript");
+            jsJquery.Attributes.Add("src", "../content/js/jquery-3.1.1.js");
+
+            Header.Controls.AddAt(0, jsJquery)
+        }
+    }
+}
